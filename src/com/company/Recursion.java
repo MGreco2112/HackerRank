e09b383007590b6339f0879d4870e219f2cfe7f1
MGreco2112 <mgreco2112@tutanota.com>
2021-07-30 18:12:09 -0400

    Finished Day 9 of 30, Recursion

---
+++ src/com/company/Recursion.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Recursion {
+
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
