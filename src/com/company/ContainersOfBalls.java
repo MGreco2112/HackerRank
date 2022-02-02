b681a3882d21e50cdc2146aa13a39e0848c1b34e
Matthew Greco <mgreco2112@tutanota.com>
2022-02-01 21:32:26 -0500

    solution started

---
+++ src/com/company/ContainersOfBalls.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ContainersOfBalls {
+
+    public static String organizingContainers(List<List<Integer>> container) {
+        /*
+            Map the set capacity of each bin
+            Count the occurrence of each ball in an Array
+            Iterate through bin map using the count of the balls to make sure each bin can hold the balls
+            Return Impossible if conditions are not met
+         */
+        Map<Integer, Integer> bins = new HashMap<>();
+        Map<Integer, Integer> ballCounter = new HashMap<>();
+
+        for (int i = 0; i < container.size(); i++) {
+
+            if (bins.get(i) == null) {
+
+                int count = 0;
+
+                for (int j = 0; j < container.get(i).size(); j++) {
+                    count+= container.get(i).get(j);
+
+                    if (ballCounter.get(j) == null) {
+                        ballCounter.put(j, container.get(i).get(j));
+                    } else {
+                        int ballCount = ballCounter.get(j);
+                        ballCount += container.get(i).get(j);
+
+                        ballCounter.replace(j, ballCount);
+                    }
+                }
+
+                bins.put(i, count);
+            }
+
+
+        }
+
+
+        System.out.println(bins);
+        System.out.println(ballCounter);
+
+
+        /*
+        compare the count of each type of ball to the capacity of the bins
+        if a ball count fits in a bin exactly, move on
+        if fails, return Impossible
+         */
+
+
+
+        return "Possible";
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	    String str = "(()";
+//	    String str = "(()";
+//
+//        System.out.println(BalancedBrackets.isBalanced(str));
 
-        System.out.println(BalancedBrackets.isBalanced(str));
+        List<List<Integer>> container = new ArrayList<>();
+
+        container.add(new ArrayList<>());
+        container.add(new ArrayList<>());
+
+        container.get(0).add(0);
+        container.get(0).add(2);
+
+        container.get(1).add(1);
+        container.get(1).add(1);
+
+        ContainersOfBalls.organizingContainers(container);
     }
 }
