349483fba8862968b96f92e588dfed17f8cbceba
Matthew Greco <mgreco2112@tutanota.com>
2022-02-08 22:00:46 -0500

    added plan of attack for birdCount

---
+++ src/com/company/BirdCount.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BirdCount {
+    //sort incoming array ascending
+    //map array key = birdId value = occurance in array
+    //while mapping generate largest occurance value
+    //interate through map looking for value == largest
+    //return first occurance of largest
+}
