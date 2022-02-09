2e7d2530f67ee6462fa67fbb3e9dde19b72495f2
Matthew Greco <mgreco2112@tutanota.com>
2022-02-09 17:08:38 -0500

    finished birds

--- src/com/company/BirdCount.java
+++ src/com/company/BirdCount.java
@@ -1,9 +1,47 @@
 package com.company;
 
+import java.util.*;
+
 public class BirdCount {
     //sort incoming array ascending
     //map array key = birdId value = occurance in array
     //while mapping generate largest occurance value
     //interate through map looking for value == largest
     //return first occurance of largest
+
+    public static int migratoryBirds(List<Integer> arr) {
+        Collections.sort(arr);
+
+        int finalBirdId = 0;
+
+        int mostOccurrences = 0;
+
+        Map<Integer, Integer> birdOccurrences = new HashMap<>();
+
+        for (Integer bird : arr) {
+            if (!birdOccurrences.containsKey(bird)) {
+                birdOccurrences.put(bird, 1);
+            } else {
+                int count = birdOccurrences.get(bird);
+
+                count++;
+
+                birdOccurrences.put(bird, count);
+
+                if (count > mostOccurrences) {
+                    mostOccurrences = count;
+                }
+            }
+        }
+
+        for (Integer bird : new TreeSet<Integer>(birdOccurrences.keySet())) {
+            if (birdOccurrences.get(bird) == mostOccurrences) {
+                finalBirdId = bird;
+                break;
+            }
+        }
+
+        return finalBirdId;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,25 +9,36 @@ public class Main {
 //	    String str = "(()";
 //
 //        System.out.println(BalancedBrackets.isBalanced(str));
+//
+//        List<List<Integer>> container = new ArrayList<>();
+//
+//        container.add(new ArrayList<>());
+//        container.add(new ArrayList<>());
+//        container.add(new ArrayList<>());
+//
+//        container.get(0).add(0);
+//        container.get(0).add(2);
+//        container.get(0).add(1);
+//
+//        container.get(1).add(1);
+//        container.get(1).add(1);
+//        container.get(1).add(1);
+//
+//        container.get(2).add(2);
+//        container.get(2).add(0);
+//        container.get(2).add(0);
+//
+//        System.out.println(ContainersOfBalls.organizingContainers(container));
 
-        List<List<Integer>> container = new ArrayList<>();
+        List<Integer> birds = new ArrayList<>();
 
-        container.add(new ArrayList<>());
-        container.add(new ArrayList<>());
-        container.add(new ArrayList<>());
+        birds.add(1);
+        birds.add(1);
+        birds.add(1);
+        birds.add(2);
+        birds.add(2);
+        birds.add(2);
 
-        container.get(0).add(0);
-        container.get(0).add(2);
-        container.get(0).add(1);
-
-        container.get(1).add(1);
-        container.get(1).add(1);
-        container.get(1).add(1);
-
-        container.get(2).add(2);
-        container.get(2).add(0);
-        container.get(2).add(0);
-
-        System.out.println(ContainersOfBalls.organizingContainers(container));
+        System.out.println(BirdCount.migratoryBirds(birds));
     }
 }
