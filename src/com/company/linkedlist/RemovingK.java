123739164b50968b1909f4b02b3a57343f1a42c4
Matthew Greco <mgreco2112@tutanota.com>
2022-02-18 18:11:12 -0500

    working solution finally generated

--- src/com/company/linkedlist/RemovingK.java
+++ src/com/company/linkedlist/RemovingK.java
@@ -3,9 +3,7 @@ package com.company.linkedlist;
 public class RemovingK {
 
 
-    //Set each return value to = l.next;
-
-    public static ListNode solution(ListNode l, int k) {
+    ListNode solution(ListNode l, int k) {
     /*
     Check if current l.value == k
         if so and l.next != null, set variable to l.next, set l to null, run method on var containing l.next
@@ -14,43 +12,32 @@ public class RemovingK {
         else run method on l.next
     */
 
-         System.out.println(l);
 
         if (l == null) {
-            System.out.println("This is null");
-            return l;
-        } else if (l.value == k && l.next != null) {
-            System.out.println("Next is not Null");
+            return null;
+        }
 
-             System.out.println(l.value);
+        // System.out.println(l.value);
 
-            System.out.println(l.value == 3);
+        if (l.value == k && l.next != null) {
+            // System.out.println(l.value == 3);
 
             ListNode next = l.next;
 
-            l = null;
-
             return solution(next, k);
         } else if (l.value == k) {
-            System.out.println("Next is Null");
 
-             System.out.println(l.value);
 
-            l = null;
+            return null;
+        } else if (l.next == null) {
 
             return l;
         } else {
-             System.out.println("Not Removed");
-             System.out.println(l.value);
 
-            solution(l.next, k);
+            l.next = solution(l.next, k);
         }
 
-        System.out.println("Results not removed");
-        System.out.println(l.value);
+
         return l;
-
     }
-
-
 }
