3cfef724c205305e9feed5753d0a7b1b018ac15c
Matthew Greco <mgreco2112@tutanota.com>
2022-02-22 17:15:47 -0500

    finished solution

--- src/com/company/linkedlist/MergingLinkedLists.java
+++ src/com/company/linkedlist/MergingLinkedLists.java
@@ -7,25 +7,41 @@ import java.util.List;
 public class MergingLinkedLists {
 
     public static ListNode solution(ListNode l1, ListNode l2) {
-        List<ListNode> listA = listBuilder(l1);
-        List<ListNode> listB = listBuilder(l2);
+        List<Integer> listA = listBuilder(l1);
+        List<Integer> listB = listBuilder(l2);
+
+        if (listA.size() == 0 && listB.size() == 0) {
+            return null;
+        }
 
         listA.addAll(listB);
 
+        Collections.sort(listA);
+
+        ListNode head = new ListNode(listA.get(0));
+
+        ListNode pointer = head;
+
+        for (int i = 1; i < listA.size(); i++) {
+            ListNode newNode = new ListNode(listA.get(i));
+
+            pointer.next = newNode;
+
+            pointer = newNode;
+        }
 
 
-
-        return null;
+        return head;
     }
 
 
-    private static List<ListNode> listBuilder(ListNode l) {
+    private static List<Integer> listBuilder(ListNode l) {
         if (l == null) {
-            return new ArrayList<ListNode>();
+            return new ArrayList<Integer>();
         } else {
-            List<ListNode> list = listBuilder(l.next);
+            List<Integer> list = listBuilder(l.next);
 
-            list.add(l);
+            list.add(l.value);
 
             return list;
         }
