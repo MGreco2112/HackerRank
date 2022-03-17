ec05b983eee48cfe87bd97ee1db2af39aef3851b
Matthew Greco <mgreco2112@tutanota.com>
2022-03-17 19:05:10 -0400

    finished solution

---
+++ src/com/company/MoreLinkedLists.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class MoreLinkedLists {
+//    public static Node removeDuplicates(Node head) {
+//        //Write your code here
+//
+//        Set<Integer> values = new HashSet<>();
+//
+//        Node pointer = head;
+//
+//        while (pointer != null) {
+//            values.add(pointer.data);
+//
+//            pointer = pointer.next;
+//        }
+//
+//        List<Integer>sortedSet = new ArrayList<>(values);
+//
+//        Collections.sort(sortedSet);
+//
+//        Node newPointer = null;
+//        Node newHead = null;
+//
+//        for (Integer num : sortedSet) {
+//            Node newNode = new Node(num);
+//
+//            if (newPointer != null) {
+//                newPointer.next = newNode;
+//                newPointer = newNode;
+//            } else {
+//                newHead = newNode;
+//                newPointer = newNode;
+//            }
+//
+//        }
+//
+//
+//        return newHead;
+//
+//    }
+}
