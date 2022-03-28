98c98befe41d4b6b4dd2359bf6d06b1fd8054988
Matthew Greco <mgreco2112@tutanota.com>
2022-03-28 19:44:06 -0400

    updated with IntelliJ hints

--- src/com/company/ClimbingTheLeaderboard.java
+++ src/com/company/ClimbingTheLeaderboard.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class ClimbingTheLeaderboard {
 
@@ -11,22 +12,21 @@ public class ClimbingTheLeaderboard {
         List<Integer> output = new ArrayList<>();
 
 
-
-        for (int playerPointer = 0; playerPointer < player.size(); playerPointer++) {
+        for (Integer integer : player) {
             int place = 1;
 
-            ranked.add(player.get(playerPointer));
+            ranked.add(integer);
 
             Collections.sort(ranked);
 
-            for (int scorePointer = ranked.size() - 1; scorePointer > - 1; scorePointer--) {
+            for (int scorePointer = ranked.size() - 1; scorePointer > -1; scorePointer--) {
                 if (scorePointer != ranked.size() - 1) {
                     if (ranked.get(scorePointer + 1) > ranked.get(scorePointer)) {
                         place++;
                     }
                 }
 
-                if (player.get(playerPointer) == ranked.get(scorePointer)) {
+                if (Objects.equals(integer, ranked.get(scorePointer))) {
                     output.add(place);
                     break;
                 }
