41904ffc78a954cd2b431b90ab20d1f19048fcc8
Matthew Greco <mgreco2112@tutanota.com>
2022-08-30 18:30:29 -0400

    finished BSTLevelOrderTraversal

---
+++ src/com/company/BSTLevelOrderTraversal.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+public class BSTLevelOrderTraversal {
+
+    class Solution{
+
+        static void levelOrder(Node root){
+            Queue<Node> nodeQueue = new LinkedList<>();
+
+            nodeQueue.add(root);
+
+            do {
+                Node pointer = nodeQueue.remove();
+
+                if (pointer.left != null) {
+                    nodeQueue.add(pointer.left);
+                }
+                if (pointer.right != null) {
+                    nodeQueue.add(pointer.right);
+                }
+
+                System.out.print(pointer.data + " ");
+            } while (!nodeQueue.isEmpty());
+
+        }
+
+        public static Node insert(Node root,int data){
+            if(root==null){
+                return new Node(data);
+            }
+            else{
+                Node cur;
+                if(data<=root.data){
+                    cur=insert(root.left,data);
+                    root.left=cur;
+                }
+                else{
+                    cur=insert(root.right,data);
+                    root.right=cur;
+                }
+                return root;
+            }
+        }
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }
+    }
+}
