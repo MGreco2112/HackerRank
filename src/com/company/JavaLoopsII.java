1436a47f00483459649879500fff6f46c96b151f
Matthew Greco <mgreco2112@tutanota.com>
2023-08-08 16:06:00 -0400

    completed JavaLoopsII

---
+++ src/com/company/JavaLoopsII.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class JavaLoopsII {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            String aValues = "";
+
+            for (int exp = 0; exp < n; exp++) {
+                a += Math.pow(2, exp) * b;
+                aValues += a + " ";
+            }
+
+            System.out.println(aValues);
+        }
+        in.close();
+}
